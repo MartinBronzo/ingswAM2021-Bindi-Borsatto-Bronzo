cf9707cf599dfa48d6d0b24ffeb6235ce9460a53
MartinBronzo <martin.bronzo@mail.polimi.it>
2021-05-04 16:32:25 +0200

    adding states

--- src/main/java/it/polimi/ingsw/controller/enums/GameState.java
+++ src/main/java/it/polimi/ingsw/controller/enums/GameState.java
@@ -1,4 +1,8 @@
 package it.polimi.ingsw.controller.enums;
 
 public enum GameState {
+    STARTED,
+    WAITING4PLAYERS,
+    LASTTURN,
+    ENDED
 }
--- src/main/java/it/polimi/ingsw/controller/enums/PlayerState.java
+++ src/main/java/it/polimi/ingsw/controller/enums/PlayerState.java
@@ -1,4 +1,11 @@
 package it.polimi.ingsw.controller.enums;
 
 public enum PlayerState {
+    DISCONNECTED,
+    WAITING4TURN,
+    PLAYING,
+    WAITING4LASTTURN,
+    PLAYINGLASTTURN,
+    WAITING4GAMEEND,
+    WAITINGGAMESTART
 }
