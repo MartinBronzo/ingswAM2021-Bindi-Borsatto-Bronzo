b9b95eac44f8fbd438107a2689d80ddd7497a375
LudovicaBindi <ludovica.bindi@mail.polimi.it>
2021-05-08 09:51:00 +0200

    Added a new package.

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -1,5 +1,31 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.model.MainBoard;
+import it.polimi.ingsw.model.PlayerBoard;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class GameController {
-    private ClientHandler actualActive;
+    private ClientHandler activePlayer;
+    private MainBoard mainBoard;
+    private Map<ClientHandler, PlayerBoard> players;
+
+    /*
+    ###########################################################################################################
+     GENERAL GETTERS
+    ###########################################################################################################
+     */
+
+    public List<ClientHandler> getPlayersList(){
+        List<ClientHandler> result = new ArrayList<>();
+        for (Map.Entry<ClientHandler, PlayerBoard> entry : players.entrySet())
+            result.add(entry.getKey());
+        return result;
+    }
+
+
+
+
 }
