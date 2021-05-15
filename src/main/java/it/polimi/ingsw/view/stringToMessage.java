8bac9263c4ea823a97c2f7c0c7dc7d18fc0de082
MartinBronzo <martin.bronzo@mail.polimi.it>
2021-05-15 19:01:18 +0200

    Implemented All methods to convert from user cli text to server Messages

--- src/main/java/it/polimi/ingsw/view/stringToMessage.java
+++ src/main/java/it/polimi/ingsw/view/stringToMessage.java
@@ -1,12 +1,15 @@
 package it.polimi.ingsw.view;
 
-import it.polimi.ingsw.network.messages.fromClient.GetFromMatrixMessage;
+import it.polimi.ingsw.model.ResourceType;
+import it.polimi.ingsw.network.messages.fromClient.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class stringToMessage {
+
     public static GetFromMatrixMessage toMatrixMessage(String string) throws IllegalArgumentException{
         int colNumber;
         int rowNumber;
@@ -23,11 +26,242 @@ public class stringToMessage {
             } else {
                 throw new IllegalArgumentException("String is not well formatted");
             }
-            String[] leaderIds = infos[1].split("\\s+");
-            leaderList = Arrays.stream(leaderIds).filter(id -> id.length() != 0).map(Integer::parseInt).collect(Collectors.toList());
+            String[] leaderIds = infos[1].split(",");
+            leaderList = Arrays.stream(leaderIds).map(String::strip).filter(id -> id.length() != 0).map(Integer::parseInt).collect(Collectors.toList());
         } catch (Exception e){
             throw new IllegalArgumentException("String is not well formatted");
         }
         return new GetFromMatrixMessage(rowNumber, colNumber, leaderList);
     }
+
+    public static BuyDevCardMessage toBuyDevCardMessage(String string) throws IllegalArgumentException{
+        int colNumber;
+        int rowNumber;
+        List<Integer> leaderList;
+        List<DepotParams> depotParamsList;
+        HashMap<ResourceType,Integer> leaderMap;
+        HashMap<ResourceType,Integer> strongboxMap;
+        int devslot;
+        try {
+            String[] infos = string.split(";");
+            String[] rcInfos = infos[0].split("\\s+");
+            if (rcInfos[0].equals("row")) {
+                rowNumber = Integer.parseInt(rcInfos[1]);
+                colNumber = 0;
+            } else if (rcInfos[0].equals("column")) {
+                rowNumber = 0;
+                colNumber = Integer.parseInt(rcInfos[1]);
+            } else {
+                throw new IllegalArgumentException("String is not well formatted");
+            }
+            String[] leaderIds = infos[1].split(",");
+            leaderList = Arrays.stream(leaderIds).map(String::strip).filter(id -> id.length() != 0).map(Integer::parseInt).collect(Collectors.toList());
+            depotParamsList = Arrays.stream(infos[2].split(",")).map(stringToMessage::toDepotParams).collect(Collectors.toList());
+            leaderMap = toResourceHashMap(infos[3]);
+            strongboxMap = toResourceHashMap(infos[4]);
+            devslot = Integer.parseInt(infos[5].strip());
+        } catch (Exception e){
+            throw new IllegalArgumentException("String is not well formatted");
+        }
+        return new BuyDevCardMessage(rowNumber, colNumber, leaderList, depotParamsList, leaderMap, strongboxMap, devslot);
+    }
+
+    public static BuyFromMarketMessage toBuyFromMarketMessage(String string) throws IllegalArgumentException{
+        int colNumber;
+        int rowNumber;
+        List<Integer> leaderList;
+        List<DepotParams> depotParamsList;
+        HashMap<ResourceType,Integer> leaderMap;
+        HashMap<ResourceType,Integer> discardMap;
+        try {
+            String[] infos = string.split(";");
+            String[] rcInfos = infos[0].split("\\s+");
+            if (rcInfos[0].equals("row")) {
+                rowNumber = Integer.parseInt(rcInfos[1]);
+                colNumber = 0;
+            } else if (rcInfos[0].equals("column")) {
+                rowNumber = 0;
+                colNumber = Integer.parseInt(rcInfos[1]);
+            } else {
+                throw new IllegalArgumentException("String is not well formatted");
+            }
+            String[] leaderIds = infos[1].split(",");
+            leaderList = Arrays.stream(leaderIds).map(String::strip).filter(id -> id.length() != 0).map(Integer::parseInt).collect(Collectors.toList());
+            depotParamsList = Arrays.stream(infos[2].split(",")).map(stringToMessage::toDepotParams).collect(Collectors.toList());
+            leaderMap = toResourceHashMap(infos[3]);
+            discardMap = toResourceHashMap(infos[4]);
+        } catch (Exception e){
+            throw new IllegalArgumentException("String is not well formatted");
+        }
+        return new BuyFromMarketMessage(rowNumber, colNumber, leaderList, depotParamsList, leaderMap, discardMap);
+    }
+
+    public static ActivateProductionMessage toActivateProductionMessage(String string) throws IllegalArgumentException{
+        List<Integer> devCardList;
+        HashMap<Integer, ResourceType> leaderHashMap;
+        BaseProductionParams baseProductionParams;
+        List<DepotParams> depotParamsList;
+        HashMap<ResourceType,Integer> leaderSlotMap;
+        HashMap<ResourceType,Integer> strongboxMap;
+        try {
+            String[] infos = string.split(";");
+            String[] devCardsIds = infos[0].split(",");
+            devCardList = Arrays.stream(devCardsIds).map(String::strip).filter(id -> id.length() != 0).map(Integer::parseInt).collect(Collectors.toList());
+            leaderHashMap = toLeadersHashMap(infos[1]);
+            baseProductionParams = toBaseProductionParams(infos[2]);
+            depotParamsList = Arrays.stream(infos[3].split(",")).map(stringToMessage::toDepotParams).collect(Collectors.toList());
+            leaderSlotMap = toResourceHashMap(infos[4]);
+            strongboxMap = toResourceHashMap(infos[5]);
+        } catch (Exception e){
+            throw new IllegalArgumentException("String is not well formatted");
+        }
+        return new ActivateProductionMessage(devCardList, leaderHashMap, baseProductionParams, depotParamsList, leaderSlotMap, strongboxMap);
+    }
+
+    public static GetProductionCostMessage toGetProductionCostMessage(String string) throws IllegalArgumentException{
+        List<Integer> devCardList;
+        List<Integer> leaderList;
+        BaseProductionParams baseProductionParams;
+        try {
+            String[] infos = string.split(";");
+            devCardList = Arrays.stream(infos[0].split(",")).map(String::strip).filter(id -> id.length() != 0).map(Integer::parseInt).collect(Collectors.toList());
+            leaderList = Arrays.stream(infos[1].split(",")).map(String::strip).filter(id -> id.length() != 0).map(Integer::parseInt).collect(Collectors.toList());
+            baseProductionParams = toBaseProductionParams(infos[2]);
+        } catch (Exception e){
+            throw new IllegalArgumentException("String is not well formatted");
+        }
+        return new GetProductionCostMessage(devCardList, leaderList, baseProductionParams);
+    }
+
+    public static DepotParams toDepotParams(String string) throws IllegalArgumentException{
+        ResourceType resource;
+        int qt;
+        int shelf;
+        try {
+            String[] infos = Arrays.stream(string.split("\\s+")).filter(subString -> subString.length() != 0).toArray(String[]::new);
+            resource = ResourceType.valueOf(infos[0].strip());
+            qt = Integer.parseInt(infos[1].strip());
+            shelf = Integer.parseInt(infos[2].strip());
+        } catch (Exception e){
+            throw new IllegalArgumentException("String is not well formatted");
+        }
+        return new DepotParams(resource, qt, shelf);
+    }
+
+    public static HashMap<ResourceType,Integer> toResourceHashMap(String string) throws IllegalArgumentException{
+        ResourceType resource;
+        int qt;
+        List<String> subStrings;
+        HashMap<ResourceType,Integer> hashMap = new HashMap<>();
+        try {
+            String[] infos = Arrays.stream(string.split(",")).filter(subString -> subString.length() != 0).toArray(String[]::new);
+            for (String info:infos) {
+                subStrings = Arrays.stream(info.split("\\s+")).map(String::strip).filter(sub -> sub.length() != 0).collect(Collectors.toList());
+                resource = ResourceType.valueOf(subStrings.get(0));
+                qt = Integer.parseInt(subStrings.get(1));
+                hashMap.put(resource, qt);
+            }
+        } catch (Exception e){
+            throw new IllegalArgumentException("String is not well formatted");
+        }
+        return hashMap;
+    }
+
+    public static HashMap<Integer,ResourceType> toLeadersHashMap(String string) throws IllegalArgumentException{
+        ResourceType resource;
+        int leaderId;
+        List<String> subStrings;
+        HashMap<Integer,ResourceType> hashMap = new HashMap<>();
+        try {
+            String[] infos = Arrays.stream(string.split(",")).filter(subString -> subString.length() != 0).toArray(String[]::new);
+            for (String info:infos) {
+                subStrings = Arrays.stream(info.split("\\s+")).map(String::strip).filter(sub -> sub.length() != 0).collect(Collectors.toList());
+                resource = ResourceType.valueOf(subStrings.get(1));
+                leaderId = Integer.parseInt(subStrings.get(0));
+                hashMap.put(leaderId, resource);
+            }
+        } catch (Exception e){
+            throw new IllegalArgumentException("String is not well formatted");
+        }
+        return hashMap;
+    }
+
+    public static BaseProductionParams toBaseProductionParams(String string) throws IllegalArgumentException{
+        Boolean activated;
+        List<ResourceType> resourceInput;
+        List<ResourceType> resourceOutput;
+        try {
+            String[] infos = Arrays.stream(string.split(",")).map(String::strip).toArray(String[]::new);
+            activated = Boolean.parseBoolean(infos[0]);
+            resourceInput = Arrays.stream(infos[1].split("\\s+")).filter(subString -> subString.length() != 0).map(ResourceType::valueOf).collect(Collectors.toList());
+            resourceOutput = Arrays.stream(infos[2].split("\\s+")).filter(subString -> subString.length() != 0).map(ResourceType::valueOf).collect(Collectors.toList());
+        } catch (Exception e){
+            throw new IllegalArgumentException("String is not well formatted");
+        }
+        return new BaseProductionParams(activated,resourceInput,resourceOutput);
+    }
+
+    public static DiscardLeaderAndExtraResBeginningMessage toDiscardLeaderAndExtraResBeginningMessage(String string) throws IllegalArgumentException{
+        List<Integer> leaderCardIds;
+        List<DepotParams> depotParamsList;
+        try {
+            String[] infos = Arrays.stream(string.split(";")).map(String::strip).toArray(String[]::new);
+            leaderCardIds = Arrays.stream(infos[0].split(",")).map(String::strip).filter(id -> id.length() != 0).map(Integer::parseInt).collect(Collectors.toList());
+            depotParamsList = Arrays.stream(infos[1].split(",")).map(stringToMessage::toDepotParams).collect(Collectors.toList());
+            } catch (Exception e){
+            throw new IllegalArgumentException("String is not well formatted");
+        }
+        return new DiscardLeaderAndExtraResBeginningMessage(leaderCardIds, depotParamsList);
+    }
+
+    public static LeaderMessage toLeaderMessage(String string) throws IllegalArgumentException{
+        int leaderId;
+        try {
+            String[] infos = Arrays.stream(string.split(";")).map(String::strip).toArray(String[]::new);
+            leaderId = Integer.parseInt(infos[0]);
+        } catch (Exception e){
+            throw new IllegalArgumentException("String is not well formatted");
+        }
+        return new LeaderMessage(leaderId);
+    }
+
+    public static MoveBetweenShelvesMessage toMoveBetweenShelvesMessage(String string) throws IllegalArgumentException{
+        int sourceShelf, destShelf;
+        try {
+            String[] infos = Arrays.stream(string.split(";")).map(String::strip).toArray(String[]::new);
+            sourceShelf = Integer.parseInt(infos[0]);
+            destShelf = Integer.parseInt(infos[1]);
+        } catch (Exception e){
+            throw new IllegalArgumentException("String is not well formatted");
+        }
+        return new MoveBetweenShelvesMessage(sourceShelf, destShelf);
+    }
+
+    public static MoveLeaderToShelfMessage toMoveLeaderToShelfMessage(String string) throws IllegalArgumentException{
+        ResourceType resource;
+        int quantity, destShelf;
+        try {
+            String[] infos = Arrays.stream(string.split(";")).map(String::strip).toArray(String[]::new);
+            resource = ResourceType.valueOf(infos[0]);
+            quantity = Integer.parseInt(infos[1]);
+            destShelf = Integer.parseInt(infos[2]);
+        } catch (Exception e){
+            throw new IllegalArgumentException("String is not well formatted");
+        }
+        return new MoveLeaderToShelfMessage(resource, quantity, destShelf);
+    }
+
+    public static MoveShelfToLeaderMessage toMoveShelfToLeaderMessage(String string) throws IllegalArgumentException{
+        int numShelf, quantity;
+        try {
+            String[] infos = Arrays.stream(string.split(";")).map(String::strip).toArray(String[]::new);
+            numShelf = Integer.parseInt(infos[0]);
+            quantity = Integer.parseInt(infos[1]);
+        } catch (Exception e){
+            throw new IllegalArgumentException("String is not well formatted");
+        }
+        return new MoveShelfToLeaderMessage(numShelf, quantity);
+    }
+
+
 }
--- src/test/java/it/polimi/ingsw/view/stringToMessageTest.java
+++ src/test/java/it/polimi/ingsw/view/stringToMessageTest.java
@@ -1,11 +1,13 @@
 package it.polimi.ingsw.view;
 
-import it.polimi.ingsw.network.messages.fromClient.GetFromMatrixMessage;
-import it.polimi.ingsw.network.messages.fromClient.Message;
+import it.polimi.ingsw.model.ResourceType;
+import it.polimi.ingsw.network.messages.fromClient.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,7 +15,7 @@ class stringToMessageTest {
 
     @Test
     void toMatrixMessageTest() {
-        String string = "row 3; 1 2 4;";
+        String string = "row 3; 1, 2, 4;";
         GetFromMatrixMessage message = stringToMessage.toMatrixMessage(string);
         assertEquals(0, message.getCol());
         assertEquals(3, message.getRow());
@@ -21,5 +23,142 @@ class stringToMessageTest {
         assertEquals(1, integers.get(0));
         assertEquals(2, integers.get(1));
         assertEquals(4, integers.get(2));
+    }
+
+    @Test
+    void toBuyDevCardMessage() {
+        String string = "row 3; 1, 2, 4; COIN 2 2, STONE 2 2; COIN 2, SERVANT 1; STONE 2, SERVANT 2; 4;";
+        BuyDevCardMessage message = stringToMessage.toBuyDevCardMessage(string);
+        assertEquals(0, message.getCol());
+        assertEquals(3, message.getRow());
+        List<Integer> integers = message.getLeaders();
+        assertEquals(1, integers.get(0));
+        assertEquals(2, integers.get(1));
+        assertEquals(4, integers.get(2));
+        List<DepotParams> depotParamsList = message.getDepotRes();
+        assertEquals(ResourceType.STONE, depotParamsList.get(1).getResourceType());
+        assertEquals(2, depotParamsList.get(1).getQt());
+        assertEquals(2, depotParamsList.get(1).getShelf());
+        assertEquals(4, message.getDevSlot());
+        HashMap leaderMap = message.getLeaderRes();
+        assertEquals(2, leaderMap.get(ResourceType.COIN));
+        assertEquals(1, leaderMap.get(ResourceType.SERVANT));
+        HashMap stronboxMap = message.getStrongboxRes();
+        assertEquals(2, stronboxMap.get(ResourceType.STONE));
+        assertEquals(2, stronboxMap.get(ResourceType.SERVANT));
+    }
+
+    @Test
+    void toBuyFromMarketMessage() {
+        String string = "row 3; 1, 2, 4; COIN 2 2, STONE 2 2; COIN 2, SERVANT 1; STONE 2, SERVANT 2;";
+        BuyFromMarketMessage message = stringToMessage.toBuyFromMarketMessage(string);
+        assertEquals(0, message.getCol());
+        assertEquals(3, message.getRow());
+        List<Integer> integers = message.getLeaderList();
+        assertEquals(1, integers.get(0));
+        assertEquals(2, integers.get(1));
+        assertEquals(4, integers.get(2));
+        List<DepotParams> depotParamsList = message.getDepotRes();
+        assertEquals(ResourceType.STONE, depotParamsList.get(1).getResourceType());
+        assertEquals(2, depotParamsList.get(1).getQt());
+        assertEquals(2, depotParamsList.get(1).getShelf());
+        Map leaderMap = message.getLeaderRes();
+        assertEquals(2, leaderMap.get(ResourceType.COIN));
+        assertEquals(1, leaderMap.get(ResourceType.SERVANT));
+        Map stronboxMap = message.getDiscardRes();
+        assertEquals(2, stronboxMap.get(ResourceType.STONE));
+        assertEquals(2, stronboxMap.get(ResourceType.SERVANT));
+    }
+
+    @Test
+    void toActivateProductionMessage() {
+        String string = "1, 2, 4; 2 COIN, 1 SERVANT; TRUE, COIN SERVANT, STONE; COIN 2 2, STONE 2 2;  COIN 2, SERVANT 1; STONE 2, SERVANT 2;";
+        ActivateProductionMessage message = stringToMessage.toActivateProductionMessage(string);
+        List<Integer> integers = message.getDevCards();
+        assertEquals(1, integers.get(0));
+        assertEquals(2, integers.get(1));
+        assertEquals(4, integers.get(2));
+        HashMap<Integer,ResourceType> leaderIdsMap = message.getLeaders();
+        assertEquals(ResourceType.COIN, leaderIdsMap.get(2));
+        assertEquals(ResourceType.SERVANT, leaderIdsMap.get(1));
+        assertTrue(message.getBaseProduction().isActivated());
+        assertEquals(ResourceType.COIN, message.getBaseProduction().getBaseInput().get(0));
+        assertEquals(ResourceType.SERVANT, message.getBaseProduction().getBaseInput().get(1));
+        assertEquals(ResourceType.STONE, message.getBaseProduction().getBaseOutput().get(0));
+        List<DepotParams> depotParamsList = message.getDepotInputRes();
+        assertEquals(ResourceType.STONE, depotParamsList.get(1).getResourceType());
+        assertEquals(2, depotParamsList.get(1).getQt());
+        assertEquals(2, depotParamsList.get(1).getShelf());
+        HashMap leaderSlotRes = message.getLeaderSlotRes();
+        assertEquals(2, leaderSlotRes.get(ResourceType.COIN));
+        assertEquals(1, leaderSlotRes.get(ResourceType.SERVANT));
+        HashMap stronboxMap = message.getStrongboxInputRes();
+        assertEquals(2, stronboxMap.get(ResourceType.STONE));
+        assertEquals(2, stronboxMap.get(ResourceType.SERVANT));
+    }
+
+    @Test
+    void toDepotParamTest() {
+        String string = "COIN 2 3";
+        DepotParams message = stringToMessage.toDepotParams(string);
+        assertEquals(2, message.getQt());
+        assertEquals(3, message.getShelf());
+        assertEquals(ResourceType.COIN, message.getResourceType());
+    }
+
+    @Test
+    void toHashMapTest() {
+        String string = "COIN 2, SERVANT 1";
+        HashMap hashMap = stringToMessage.toResourceHashMap(string);
+        assertEquals(2, hashMap.get(ResourceType.COIN));
+        assertEquals(1, hashMap.get(ResourceType.SERVANT));
+    }
+
+    @Test
+    void toBaseProductionParamsTest() {
+        String string = "TRUE, COIN SERVANT, STONE";
+        BaseProductionParams message = stringToMessage.toBaseProductionParams(string);
+        assertTrue(message.isActivated());
+        assertEquals(ResourceType.COIN, message.getBaseInput().get(0));
+        assertEquals(ResourceType.SERVANT, message.getBaseInput().get(1));
+        assertEquals(ResourceType.STONE, message.getBaseOutput().get(0));
+    }
+
+    @Test
+    void toDiscardLeaderAndExtraResBeginningMessageTest() {
+        String string = "1, 2, 4; COIN 2 3, STONE 1 4;";
+        DiscardLeaderAndExtraResBeginningMessage message = stringToMessage.toDiscardLeaderAndExtraResBeginningMessage(string);
+        List<Integer> integers = message.getLeaderCard();
+        assertEquals(1, integers.get(0));
+        assertEquals(2, integers.get(1));
+        assertEquals(4, integers.get(2));
+        List<DepotParams> depotParamsList = message.getDepotRes();
+        assertEquals(ResourceType.COIN, depotParamsList.get(0).getResourceType());
+        assertEquals(2, depotParamsList.get(0).getQt());
+        assertEquals(3, depotParamsList.get(0).getShelf());
+        assertEquals(ResourceType.STONE, depotParamsList.get(1).getResourceType());
+        assertEquals(1, depotParamsList.get(1).getQt());
+        assertEquals(4, depotParamsList.get(1).getShelf());
+
+    }
+
+    @Test
+    void toGetProductionCostMessageTest() {
+        String string = "1, 2, 4; 1, 2, 4; TRUE, COIN SERVANT, STONE;";
+        GetProductionCostMessage message = stringToMessage.toGetProductionCostMessage(string);
+        List<Integer> integers = message.getDevCards();
+        assertEquals(1, integers.get(0));
+        assertEquals(2, integers.get(1));
+        assertEquals(4, integers.get(2));
+        integers = message.getLeaders();
+        assertEquals(1, integers.get(0));
+        assertEquals(2, integers.get(1));
+        assertEquals(4, integers.get(2));
+        BaseProductionParams baseProductionParams = message.getBaseProd();
+        assertTrue(baseProductionParams.isActivated());
+        assertEquals(ResourceType.COIN, baseProductionParams.getBaseInput().get(0));
+        assertEquals(ResourceType.SERVANT, baseProductionParams.getBaseInput().get(1));
+        assertEquals(ResourceType.STONE, baseProductionParams.getBaseOutput().get(0));
+
     }
 }
