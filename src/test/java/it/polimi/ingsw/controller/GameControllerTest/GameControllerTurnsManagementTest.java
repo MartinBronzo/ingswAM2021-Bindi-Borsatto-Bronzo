fd3a10ffd4114264f6c5e1ba3faf5ae7ca61c48e
LudovicaBindi <ludovica.bindi@mail.polimi.it>
2021-05-16 16:28:28 +0200

    Tested the message received by Clients when the GameController specifyNextPlayer method is called.

--- src/test/java/it/polimi/ingsw/controller/GameControllerTest/GameControllerTurnsManagementTest.java
+++ src/test/java/it/polimi/ingsw/controller/GameControllerTest/GameControllerTurnsManagementTest.java
@@ -1,18 +1,29 @@
 package it.polimi.ingsw.controller.GameControllerTest;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
 import it.polimi.ingsw.controller.ClientHandler;
 import it.polimi.ingsw.controller.GameController;
 import it.polimi.ingsw.controller.enums.PlayerState;
 import it.polimi.ingsw.exceptions.IllegalActionException;
+import it.polimi.ingsw.model.LeaderCard.LeaderCardRequirements.CardRequirementColor;
+import it.polimi.ingsw.model.LeaderCard.LeaderCardRequirements.CardRequirementColorAndLevel;
+import it.polimi.ingsw.model.LeaderCard.LeaderCardRequirements.CardRequirementResource;
+import it.polimi.ingsw.model.LeaderCard.LeaderCardRequirements.Requirement;
+import it.polimi.ingsw.model.LeaderCard.leaderEffects.*;
+import it.polimi.ingsw.network.messages.sendToClient.ModelUpdate;
+import it.polimi.ingsw.network.messages.sendToClient.ResponseMessage;
+import it.polimi.ingsw.network.messages.sendToClient.ResponseType;
+import it.polimi.ingsw.view.readOnlyModel.Game;
+import it.polimi.ingsw.view.readOnlyModel.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
 import java.net.Socket;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameControllerTurnsManagementTest {
     GameController gameController;
@@ -52,6 +63,24 @@ public class GameControllerTurnsManagementTest {
         fileReader2 = new BufferedReader(new FileReader("ClientHandler2File.json"));
         fileReader3 = new BufferedReader(new FileReader("ClientHandler3File.json"));
         fileReader4 = new BufferedReader(new FileReader("ClientHandler4File.json"));
+
+        RuntimeTypeAdapterFactory<Requirement> requirementTypeFactory
+                = RuntimeTypeAdapterFactory.of(Requirement.class, "type");
+        requirementTypeFactory.registerSubtype(Requirement.class, "requirement"); //TODO: this is only for testing purpose, in the real game we won't have requirements of type Requirement but a subtype of it
+        requirementTypeFactory.registerSubtype(CardRequirementColor.class, "cardRequirementColor");
+        requirementTypeFactory.registerSubtype(CardRequirementResource.class, "cardRequirementResource");
+        requirementTypeFactory.registerSubtype(CardRequirementColorAndLevel.class, "cardRequirementColorAndLevel");
+
+        RuntimeTypeAdapterFactory<Effect> effectTypeFactory
+                = RuntimeTypeAdapterFactory.of(Effect.class, "type");
+        effectTypeFactory.registerSubtype(Effect.class, "effect"); //TODO: this is only for testing purpose, in the real game we won't have effect of type Effect but a subtype of it
+        effectTypeFactory.registerSubtype(DiscountLeaderEffect.class, "discountLeaderEffect");
+        effectTypeFactory.registerSubtype(ExtraSlotLeaderEffect.class, "extraSlotLeaderEffect");
+        effectTypeFactory.registerSubtype(WhiteMarbleLeaderEffect.class, "whiteMarbleLeaderEffect");
+        effectTypeFactory.registerSubtype(ExtraProductionLeaderEffect.class, "extraProductionLeaderEffect");
+
+        this.gson = new GsonBuilder().registerTypeAdapterFactory((requirementTypeFactory))
+                .registerTypeAdapterFactory(effectTypeFactory).create();
     }
 
     @Test
@@ -145,4 +174,84 @@ public class GameControllerTurnsManagementTest {
         Exception e = assertThrows(IllegalStateException.class, ()->gameController.specifyNextPlayer(c2));
         assertEquals(e.getMessage(), "All the players are disconnected!");
     }*/
+
+    @Test
+    public void ctrlUpdateMessageNoDisconnection() throws IOException {
+        c1.setPlayerState(PlayerState.WAITING4TURN);
+        c2.setPlayerState(PlayerState.WAITING4TURN);
+        c3.setPlayerState(PlayerState.PLAYING);
+        c4.setPlayerState(PlayerState.WAITING4TURN);
+
+        gameController.specifyNextPlayer(c3);
+
+        String res1 = fileReader1.readLine();
+        String res2 = fileReader2.readLine();
+        String res3 = fileReader3.readLine();
+        String res4 = fileReader4.readLine();
+
+        assertEquals(res1, res2);
+        assertEquals(res2, res3);
+        assertEquals(res3, res4);
+
+        ResponseMessage responseMessage = gson.fromJson(res1, ResponseMessage.class);
+        assertEquals(responseMessage.getResponseType(), ResponseType.UPDATE);
+        ModelUpdate update = gson.fromJson(responseMessage.getResponseContent(), ModelUpdate.class);
+        Game game = update.getGame();
+
+        Player player;
+
+        assertEquals(game.getPlayers().size(), 4);
+
+        player = game.getPlayers().stream().filter(x -> x.getNickName().equals("Client 1")).findFirst().get();
+        assertEquals(player.getPlayerState(), PlayerState.WAITING4TURN);
+
+        player = game.getPlayers().stream().filter(x -> x.getNickName().equals("Client 2")).findFirst().get();
+        assertEquals(player.getPlayerState(), PlayerState.WAITING4TURN);
+
+        player = game.getPlayers().stream().filter(x -> x.getNickName().equals("Client 3")).findFirst().get();
+        assertEquals(player.getPlayerState(), PlayerState.WAITING4TURN);
+
+        player = game.getPlayers().stream().filter(x -> x.getNickName().equals("Client 4")).findFirst().get();
+        assertEquals(player.getPlayerState(), PlayerState.PLAYING);
+    }
+
+    @Test
+    public void ctrlUpdateMessageOneDisconnection() throws IOException {
+        c1.setPlayerState(PlayerState.WAITING4TURN);
+        c2.setPlayerState(PlayerState.PLAYING);
+        c3.setPlayerState(PlayerState.DISCONNECTED);
+        c4.setPlayerState(PlayerState.WAITING4TURN);
+
+        gameController.specifyNextPlayer(c2);
+
+        String res1 = fileReader1.readLine();
+        String res2 = fileReader2.readLine();
+        String res4 = fileReader4.readLine();
+
+        //We don't read what the third client receives because they are disconnected!
+        assertEquals(res1, res2);
+        assertEquals(res2, res4);
+
+        ResponseMessage responseMessage = gson.fromJson(res1, ResponseMessage.class);
+        assertEquals(responseMessage.getResponseType(), ResponseType.UPDATE);
+        ModelUpdate update = gson.fromJson(responseMessage.getResponseContent(), ModelUpdate.class);
+        Game game = update.getGame();
+
+        Player player;
+
+        assertEquals(game.getPlayers().size(), 4);
+
+        player = game.getPlayers().stream().filter(x -> x.getNickName().equals("Client 1")).findFirst().get();
+        assertEquals(player.getPlayerState(), PlayerState.WAITING4TURN);
+
+        player = game.getPlayers().stream().filter(x -> x.getNickName().equals("Client 2")).findFirst().get();
+        assertEquals(player.getPlayerState(), PlayerState.WAITING4TURN);
+
+        player = game.getPlayers().stream().filter(x -> x.getNickName().equals("Client 3")).findFirst().get();
+        assertEquals(player.getPlayerState(), PlayerState.DISCONNECTED);
+
+        player = game.getPlayers().stream().filter(x -> x.getNickName().equals("Client 4")).findFirst().get();
+        assertEquals(player.getPlayerState(), PlayerState.PLAYING);
+    }
+
 }
