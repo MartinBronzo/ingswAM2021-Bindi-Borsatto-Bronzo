762fafa89e6467ac2a4f590da984ee203a041fe2
MartinBronzo <martin.bronzo@mail.polimi.it>
2021-06-14 02:54:34 +0200

    add utility method to set multiline text in jlabel

---
+++ src/main/java/it/polimi/ingsw/view/gui/ViewComponents/Utils.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.view.gui.ViewComponents;
+
+import javax.swing.*;
+
+public class Utils {
+    public static void multilineJLabelSetText(JLabel label, String string) throws NullPointerException {
+        if (label==null || string == null) throw new NullPointerException();
+
+        String converted = "<html>" + string.replaceAll("\n", "<br>");
+        label.setText(converted);
+    }
+}
